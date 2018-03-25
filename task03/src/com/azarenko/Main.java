9c10d0f4dfc2a90d7047128a44251638e36c5071
AntonAzarenko <ant-azarenko@mail.ru>
2018-03-25 17:29:59 +0300

    first commit

--- task03/src/com/azarenko/Main.java
+++ task03/src/com/azarenko/Main.java
@@ -1,7 +1,5 @@
 package com.azarenko;
 
 public class Main {
-    public static void main(String[] args) {
 
-    }
 }
--- task03/src/com/azarenko/model/Site.java
+++ task03/src/com/azarenko/model/Site.java
@@ -1,8 +1,5 @@
 package com.azarenko.model;
 
 public abstract class Site {
-    @Override
-    public String toString() {
-        return super.toString();
-    }
+
 }
